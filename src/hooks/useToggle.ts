f5587ea1dc8d6159d2867fd1f328434c19b66179
tati1129 <tatka.bogat@gmail.com>
2026-03-23 04:02:17 +0100

    change page

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
-export default function useToggle(value: boolean=true){
-    const [toggle, setToggle] = useState(false);
+export default function useToggle(value: boolean = true){
+    const [toggle, setToggle] = useState(value);
 
     const handleToggle = () =>{
         setToggle((prev) => !prev);
